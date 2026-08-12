1aa5d341d5eb7de0ffaee642ab24aae12f4c1c9a
Lors2021 <94729519+Lors2021@users.noreply.github.com>
2026-08-12 18:45:11 +0500

    Create Shape.kt

---
+++ app/src/main/java/com/udarnyrezhim/presentation/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.udarnyrezhim.presentation.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val AppShapes = Shapes(
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(20.dp),
+    large = RoundedCornerShape(24.dp),
+    extraLarge = RoundedCornerShape(28.dp)
+)
