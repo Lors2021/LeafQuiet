86abbdc705e42925be7f3777ff767a14f347fc34
Lors2021 <94729519+Lors2021@users.noreply.github.com>
2026-08-12 21:36:40 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.dagger.hilt.android")
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "8.2.2" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
     id("kotlin-kapt")
 }
 
