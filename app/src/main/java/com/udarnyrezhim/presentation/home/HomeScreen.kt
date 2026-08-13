8740c3432fe36abe8598762bafc15552334c2619
Lors2021 <94729519+Lors2021@users.noreply.github.com>
2026-08-13 09:32:17 +0500

    Update HomeScreen.kt

--- app/src/main/java/com/udarnyrezhim/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/udarnyrezhim/presentation/home/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.udarnyrezhim.presentation.components.*
+import com.udarnyrezhim.presentation.theme.*
 
 @Composable
 fun HomeScreen(
